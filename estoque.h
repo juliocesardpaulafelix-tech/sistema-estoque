c92687d0077bed1f6e1cee8d3214e5d22838f613
Maria Eduarda Anjelos <eduardanjelos@gmail.com>
2026-09-14 22:46:47 -0300

    Add interest rate constant and function for interest calculation

--- estoque.h
+++ estoque.h
@@ -3,6 +3,7 @@
 
 #define MAX_ITENS 10
 #define TAXA_PADRAO 0.05
+#define TAXA_JUROS 0.08
 
 typedef struct {
     int id;
@@ -14,5 +15,6 @@ typedef struct {
 void exibir_menu(void);
 void listar_produtos(Produto lista[], int total);
 float calcular_total(Produto lista[], int total);
+float aplicar_juros(float total);
 
 #endif
