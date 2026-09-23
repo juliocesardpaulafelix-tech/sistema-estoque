a0176d73046856e6450b136d4a61aaa842813f55
Maria Eduarda Anjelos <eduardanjelos@gmail.com>
2026-09-23 02:19:26 -0300

    Update estoque.h with new constants and definitions

--- estoque.h
+++ estoque.h
@@ -1,8 +1,11 @@
-#ifndef ESTOQUE_H
+#define ESTOQUE_H
 #define ESTOQUE_H
 
-#define MAX_ITENS 10
-#define TAXA_PADRAO 0.10
+#define MAX_ITENS 50
+#define ESTOQUE_MINIMO 5
+#define TAXA_PADRAO 0.05
+#define TAXA_DESCONTO 0.05
+#define TAXA_JUROS 0.10
 
 typedef struct {
     int id;
@@ -17,5 +20,6 @@ void exibir_menu(void);
 void listar_produtos(Produto lista[], int total);
 float calcular_total(Produto lista[], int total);
 float aplicar_juros(float total);
+float aplicar_desconto(float total);
 
 #endif
